dc68cc180b501e697745221ca8aa309436076c6f
yoniem <yonimphini@gmail.com>
2023-07-26 00:05:20 -0700

    simple_shell

--- strg_split.c
+++ strg_split.c
@@ -14,7 +14,7 @@ char **strg_split(char *str)
     char **args_arr = NULL;
     int arg_count = 0, i;
     char *token;
-    const char *delimiter = " "; 
+    const char *delimiter = " ";
 
     args_arr = malloc(50 * sizeof(char *));
     if (!args_arr)
@@ -26,34 +26,34 @@ char **strg_split(char *str)
     token = strtok(str, delimiter);
     while (token != NULL)
     {
-    
         for (i = 0; token[i] != '\0'; i++)
         {
             if (token[i] == '\n')
             {
-              
                 token[i] = '\0';
             }
         }
 
-        args_arr[arg_count] = token;
-        arg_count++;
-
-      
-        if (arg_count >= 50)
+        args_arr[arg_count] = strdup(token);
+        if (!args_arr[arg_count])
         {
-            args_arr = realloc(args_arr, (arg_count + 1) * sizeof(char *));
-            if (!args_arr)
-            {
-                perror("strg_split reallocation error");
-                exit(EXIT_FAILURE);
-            }
+            perror("strg_split allocation error");
+            exit(EXIT_FAILURE);
         }
 
-        token = strtok(NULL, delimiter);
+        arg_count++;
+
+        if (arg_count >= 50)
+        {
+            fprintf(stderr, "Maximum number of words reached.\n");
+        break;
+        }
+
+      token = strtok(NULL, delimiter);
+
     }
 
-   
     args_arr[arg_count] = NULL;
+
     return (args_arr);
 }

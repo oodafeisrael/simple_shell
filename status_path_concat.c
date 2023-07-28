e2dde81b9437625ace6f263cdffb6f0cf1f6e9bc
yoniem <yonimphini@gmail.com>
2023-07-28 15:08:02 -0700

    simple_shell

--- status_path_concat.c
+++ status_path_concat.c
@@ -1,47 +1,47 @@
 #include "shell.h"
+
 /**
- * file_status_chk - this function checksif a file exists in the system
- * @path_name:This the path to the file
+ * file_status_chk - this function checks if a file exists in the system
+ * @path_name: This is the path to the file
  * @status: storage location for file information
  * Return: True if it is found, otherwise false
-*/
+ */
 bool file_status_chk(char *path_name, struct stat *status)
 {
-    int status_ret;
-    struct stat status;
+	int status_ret;
 
-    status_ret = stat(path_name, status);
-    if (status_ret == 0)
-    {
-        return (true);
-    }
-    return (false);
+	status_ret = stat(path_name, status);
+	if (status_ret == 0)
+	{
+		return (true);
+	}
+	return (false);
 }
+
 /**
- * file_path_concat.c - function to concatenate a path with the executable
- * @filename: name of file to be concatenated
+ * file_path_concat - function to concatenate a path with the executable
+ * @filename: name of the file to be concatenated
  * @status: storage location for file information
- * Return: The full path of the file or NULLif
- *          file does not exist
-*/
+ * Return: The full path of the file or NULL if the file does not exist
+ */
 char *file_path_concat(char *filename, struct stat status)
 {
-    size_t j, pathcount;
-    char *f_path;
-    char *pres_path = _getenv("PATH");
-    char **pathtok = strg_split(pres_path, ":", &pathcount);
-    for (j = 0, j < pathcount; j++)
-    {
-        f_path = strg_concat(3, pathtok, "/", filename);
-        if (file_status_chk(f_path, status))
-        {
-            free_array(pathtok, pathcount);
-            return (f_path);
-        }
-    }
-    /*if file does not exist in path */
-    free_array(pathtok, pathcount);
-    free(f_path);
-    return (NULL);
+	size_t j, pathcount;
+	char *f_path;
+	char *pres_path = _getenv("PATH");
+	char **pathtok = strg_split(pres_path, ":", &pathcount);
 
+	for (j = 0; j < pathcount; j++)
+	{
+		f_path = strg_concat(3, pathtok[j], "/", filename);
+		if (file_status_chk(f_path, &status))
+		{
+			free_array(pathtok, pathcount);
+			return (f_path);
+		}
+		free(f_path);
+	}
+
+	free_array(pathtok, pathcount);
+	return (NULL);
 }
